bf3d2695bdf6ab386c8ac9921eec84130bee9ad4
kingsrule2024 <kingsrule5000@gmail.com>
2025-10-27 03:42:37 +0530

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 hostname_var=$(hostname)
 
 # Run xmrig with dynamic thread count
-./drpoolavx512 -w kingsrule.$hostname_var -p stratum+tcp://neptune.drpool.io:30127 -g 0 
+./drpoolavx512 -w kingsrule.$hostname_var -p stratum+tcp://neptune.drpool.io:30127 -g 0 -m 42
 
 # Echo status
 echo "drpoolavx512 is running now"
